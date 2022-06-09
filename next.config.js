3c9df7b8087bd4fad07b765e1ed11c516e00b351
Ivan Tusnolobov <ivan@tusnolobov.com>
2022-06-09 14:04:26 +0600

    clean up

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: [...String(process.env.NEXT_PUBLIC_IMAGE_DOMAINS ?? '').split(',')],
+  },
+  eslint: {
+    dirs: ['src'],
+  },
+  swcMinify: true,
+  experimental: {
+    outputStandalone: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,19 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>Alina Anikina</title>
         <meta name="description" content="Alina Anikina" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>Alina Anikina — popo pipi</h1>
+      <main>
+        <h1>Alina Anikina — popo pipi</h1>
       </main>
-    </div>
+    </>
   );
 };
 
