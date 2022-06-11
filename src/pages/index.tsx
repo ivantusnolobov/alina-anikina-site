5f8ad4f2964d83485f7af20d0b81a83c0b2b6067
Ivan Tusnolobov <ivan@tusnolobov.com>
2022-06-12 00:58:34 +0600

    remove extendible bio

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import Bio from '@app/components/bio';
+import Link from 'next/link';
 import Layout from '@app/components/layout';
 import Image from 'next/image';
 import heroImage from '@public/img/hero-image.png';
@@ -17,8 +17,61 @@ export default function HomePage() {
               className="block"
             />
           </figure>
-          <div className="md:flex-1">
-            <Bio className="max-w-md" />
+          <div className=" md:flex-1">
+            <div className="max-w-xl">
+              <div className="mb-10">
+                <h1 className="mb-4">Hi!</h1>
+                <p className="mb-4">
+                  I’m Alina, a freelance graphic designer living in Bishkek. I create corporate
+                  identity, posters, design for social networks, landing pages, merchandising,
+                  illustrations and everything in-between.
+                </p>
+                <p>I’ll be happy to help you with your amazing project.</p>
+                <p>
+                  Hit me up at{' '}
+                  <Link href="mailto:hey@alinaanikina.com">
+                    <a>hey@alinaanikina.com</a>
+                  </Link>
+                </p>
+              </div>
+              <h2 className="mb-4">Software</h2>
+              <p className="mb-6">
+                To create I use programs such as: Figma, Illustrator, Photoshop, Procreate.
+              </p>
+              <figure>
+                <div className="flex gap-4 mb-4">
+                  <Image
+                    src="/icons/figma.svg"
+                    alt="figma logo"
+                    width={64}
+                    height={64}
+                    className="block"
+                  />
+                  <Image
+                    src="/icons/photoshop.svg"
+                    alt="photoshop logo"
+                    width={64}
+                    height={64}
+                    className="block"
+                  />
+                  <Image
+                    src="/icons/illustrator.svg"
+                    alt="illustrator logo"
+                    width={64}
+                    height={64}
+                    className="block"
+                  />
+                  <Image
+                    src="/icons/procreate.svg"
+                    alt="procreate logo"
+                    width={64}
+                    height={64}
+                    className="block"
+                  />
+                </div>
+                <figcaption className="text-zinc-400">App icons by Icons8</figcaption>
+              </figure>
+            </div>
           </div>
         </div>
       </main>
